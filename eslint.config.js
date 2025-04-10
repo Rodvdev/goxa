7947d0b64245c93e6218b545ed2ac7f7493806e7
Rodrigo Vásquez de Velasco <rodrigovdev01@gmail.com>
2025-04-10 12:23:15 -0500

    Refactor ESLint configuration to use ES module syntax and improve TypeScript support. Update package.json to align @typescript-eslint dependencies with the new configuration.

--- eslint.config.js
+++ eslint.config.js
@@ -1,13 +1,25 @@
-module.exports = {
-  root: true,
-  extends: [
-    'next/core-web-vitals',
-    'plugin:@typescript-eslint/recommended'
-  ],
-  parser: '@typescript-eslint/parser',
-  plugins: ['@typescript-eslint'],
-  rules: {
-    '@typescript-eslint/no-unused-vars': 'warn',
-    '@typescript-eslint/no-explicit-any': 'warn'
-  }
-} 
+import next from '@next/eslint-plugin-next';
+import typescript from '@typescript-eslint/eslint-plugin';
+import typescriptParser from '@typescript-eslint/parser';
+
+export default [
+  {
+    files: ['**/*.ts', '**/*.tsx'],
+    languageOptions: {
+      parser: typescriptParser,
+      parserOptions: {
+        project: './tsconfig.json',
+      },
+    },
+    plugins: {
+      '@typescript-eslint': typescript,
+      '@next/next': next,
+    },
+    rules: {
+      ...typescript.configs['recommended'].rules,
+      ...next.configs['recommended'].rules,
+      '@typescript-eslint/no-unused-vars': 'warn',
+      '@typescript-eslint/no-explicit-any': 'warn',
+    },
+  },
+]; 
