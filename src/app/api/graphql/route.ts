52dfe9c6ecab0ca973ddb217d84ba354b81ffa39
Rodrigo Vásquez de Velasco <rodrigovdev01@gmail.com>
2025-04-10 11:52:30 -0500

    Refactor GraphQL API route to simplify handler exports and remove session management. Update export syntax for GET and POST methods.

--- src/app/api/graphql/route.ts
+++ src/app/api/graphql/route.ts
@@ -2,22 +2,12 @@ import { ApolloServer } from '@apollo/server';
 import { startServerAndCreateNextHandler } from '@as-integrations/next';
 import { typeDefs } from './typeDefs';
 import { resolvers } from './resolvers';
-import { getServerSession } from 'next-auth';
-import { authOptions } from '@/lib/auth';
 
 const server = new ApolloServer({
   typeDefs,
   resolvers,
 });
 
-const handler = startServerAndCreateNextHandler(server, {
-  context: async () => {
-    const session = await getServerSession(authOptions);
-    return { session };
-  },
-});
+const handler = startServerAndCreateNextHandler(server);
 
-export const GET = handler;
-export const POST = handler;
-
-export default handler; 
+export { handler as GET, handler as POST }; 
