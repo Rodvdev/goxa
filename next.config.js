c0fc73a225781bd0e61e1ecbc477035dbb6468b8
Rodrigo Vásquez de Velasco <rodrigovdev01@gmail.com>
2025-04-10 12:25:33 -0500

    Add ESLint configuration to Next.js setup: disable built-in ESLint integration during builds for improved performance.

--- next.config.js
+++ next.config.js
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  eslint: {
+    ignoreDuringBuilds: true, // Disable Next.js built-in ESLint integration
+  },
 };
 
 module.exports = nextConfig; 
